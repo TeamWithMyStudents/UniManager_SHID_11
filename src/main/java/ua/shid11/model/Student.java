ea0ea586774f354802e7105339cd9dd4343a8f4d
Andriy <andriy2019acaundut@gmail.com>
2026-02-22 13:51:53 +0200

    Student Concrete Implementation

--- src/main/java/ua/shid11/model/Student.java
+++ src/main/java/ua/shid11/model/Student.java
@@ -1,4 +1,26 @@
 package ua.shid11.model;
 
-public class Student {
+public class Student extends User {
+    private String group;
+
+    public Student(String name, String surname, String group) {
+        super(name, surname);
+        this.group = group;
+    }
+
+    public Student(String name, String surname) {
+        super(name, surname);
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                super.toString() +
+                ", group='" + group + '\'' +
+                '}';
+    }
 }
--- src/main/java/ua/shid11/service/impl/StudentServiceImpl.java
+++ src/main/java/ua/shid11/service/impl/StudentServiceImpl.java
@@ -1,4 +1,30 @@
 package ua.shid11.service.impl;
 
-public class StudentServiceImpl {
+import ua.shid11.model.Student;
+import ua.shid11.model.User;
+import ua.shid11.service.StudentService;
+
+import java.util.Arrays;
+
+public class StudentServiceImpl extends UserServiceImpl implements StudentService {
+    public StudentServiceImpl() {
+        super(new Student[5]);
+    }
+
+    @Override
+    public Student[] findByGroup(String groupName) {
+        if (groupName == null) return new Student[0];
+
+        int size = 0;
+        User[] all = getAll();
+        Student[] students = new Student[all.length];
+
+        for (User u : all) {
+            if (u instanceof Student && groupName.equals(((Student) u).getGroup())) {
+                students[size] = (Student) u;
+                size++;
+            }
+        }
+        return Arrays.copyOf(students, size);
+    }
 }
