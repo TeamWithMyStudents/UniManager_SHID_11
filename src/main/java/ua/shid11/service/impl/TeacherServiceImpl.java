47f8293734ce48762703ae2555b0f893eccf66dc
Volodymyr <volodymyr@example.com>
2026-02-27 15:04:47 +0200

    Implement TeacherServiceImpl business logic
    1)Added add(User) override with salary validation and type check
    2)Implemented calculateTotalSalary() to sum all teacher salaries
    3)Implemented filterByDegree(String) with case-insensitive search
    3)Ensured type casting to Teacher is safe in loops

--- src/main/java/ua/shid11/service/impl/TeacherServiceImpl.java
+++ src/main/java/ua/shid11/service/impl/TeacherServiceImpl.java
@@ -9,6 +9,7 @@ public class TeacherServiceImpl extends UserServiceImpl implements TeacherServic
         super(new Teacher[5]);
     }
 
+
     @Override
     public void calculateTotalSalary() {
         double total = 0;
@@ -18,20 +19,40 @@ public class TeacherServiceImpl extends UserServiceImpl implements TeacherServic
             total += teacher.getSalary();
         }
 
-        System.out.println("Total salary = " + total);
+        System.out.println("Total University Budget: " + total);
     }
 
     @Override
     public void filterByDegree(String degree) {
         if (degree == null) {
+            System.out.println("Degree cannot be empty");
             return;
         }
+
+        boolean found = false;
 
         for (User user : getAll()) {
             Teacher teacher = (Teacher) user;
             if (teacher.getDegree() != null && teacher.getDegree().equals(degree)) {
                 System.out.println(teacher);
+                found = true;
             }
         }
+        if (!found) {
+            System.out.println("No Teacher with degree " + degree);
+        }
     }
-}
+
+    @Override
+    public void add(User user) {
+        if (!(user instanceof Teacher teacher)) {
+            System.out.println("Invalid user type");
+            return;
+        }
+        if (teacher.getSalary() < 0) {
+                System.out.println("Invalid Salary");
+                return;
+            }
+            super.add(user);
+        }
+    }
