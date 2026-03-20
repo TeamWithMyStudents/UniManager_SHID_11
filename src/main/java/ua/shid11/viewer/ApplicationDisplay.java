d12e9efaeb8c11bb1793f7cecb964907991267c1
Polina Prykhodko <you@example.com>
2026-03-20 23:13:31 +0200

    add exception to studentSubMenu

--- src/main/java/ua/shid11/viewer/ApplicationDisplay.java
+++ src/main/java/ua/shid11/viewer/ApplicationDisplay.java
@@ -70,12 +70,13 @@ public class ApplicationDisplay {
                 case "2" -> studentController.printAll();
                 case "3" -> {
                     System.out.print("Enter Student ID: ");
-                    if (!scanner.hasNextInt()) {
-                        System.out.println("Invalid Student ID (ID must be number)");
-                        scanner.nextLine();
+                    String input = scanner.nextLine();
+                    try {
+                        int id = Integer.parseInt(input);
+                        studentController.delete(id);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Error: Student ID must be an integer number!");
                     }
-                    int id = Integer.parseInt(scanner.nextLine());
-                    studentController.delete(id);
 
                 }
                 case "0" -> {
