b6d882702ebdbfcb80b5c488b4bb286d2205778e
Andriy <andriy2019acaundut@gmail.com>
2026-03-21 03:27:40 +0200

    I have crooked arms

--- src/main/java/ua/shid11/UniManager.java
+++ src/main/java/ua/shid11/UniManager.java
@@ -4,6 +4,7 @@ import ua.shid11.viewer.ApplicationDisplay;
 
 public class UniManager {
     public static void main(String[] args) {
-        ApplicationDisplay.start();
+        ApplicationDisplay app = new ApplicationDisplay();
+        app.start();
     }
 }
